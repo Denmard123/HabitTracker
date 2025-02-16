30b4e1599e4fa3a683dcd9df1ebc51678cdbea64
Denmard123 <dmardiyana123@gmail.com>
2025-02-16 15:11:08 +0700

    update lagi

--- src.js
+++ src.js
@@ -1,12 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://kxmnvtgnwuhdkrzzpwxi.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW52dGdud3VoZGtyenpwd3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTA3OTgsImV4cCI6MjA1NTI2Njc5OH0.l0DeaGtDKbr-EhNX5DpEUDSNtF1Y3L_Rdqn2bUC7JcA';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 document.addEventListener('DOMContentLoaded', () => {
   const mulaiButton = document.getElementById('mulai');
+  const SUPABASE_URL = 'https://kxmnvtgnwuhdkrzzpwxi.supabase.co'; 
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW52dGdud3VoZGtyenpwd3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTA3OTgsImV4cCI6MjA1NTI2Njc5OH0.l0DeaGtDKbr-EhNX5DpEUDSNtF1Y3L_Rdqn2bUC7JcA';
+
 
   // Menambahkan event listener untuk animasi tombol
   mulaiButton.addEventListener('click', () => {
@@ -741,3 +740,4 @@ if ('serviceWorker' in navigator) {
 }
 });
 
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
