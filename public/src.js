bc87ea22c20a9a350f4e1072dc5c393837cdd97c
Denmard123 <dmardiyana123@gmail.com>
2025-02-18 03:27:31 +0700

    u

--- public/src.js
+++ public/src.js
@@ -3,17 +3,6 @@ import { createClient } from '@supabase/supabase-js';
  const SUPABASE_URL = 'https://kxmnvtgnwuhdkrzzpwxi.supabase.co';
  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW52dGdud3VoZGtyenpwd3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTA3OTgsImV4cCI6MjA1NTI2Njc5OH0.l0DeaGtDKbr-EhNX5DpEUDSNtF1Y3L_Rdqn2bUC7JcA';
  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
- async function fetchData() {
-   const { data, error } = await supabase
-     .from('HT')
-     .select('*');
-   if (error) {
-     console.error(error);
-   } else {
-     console.log(data);
-   }
- }
- fetchData();
 document.addEventListener('DOMContentLoaded', () => {
   const mulaiButton = document.getElementById('mulai');
   // Menambahkan event listener untuk animasi tombol
