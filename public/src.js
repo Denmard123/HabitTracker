22096d9120f83e36f98a1c817d393d3bba4c2b05
Denmard123 <dmardiyana123@gmail.com>
2025-02-18 04:51:04 +0700

    kl

--- public/src.js
+++ public/src.js
@@ -2,7 +2,14 @@
 const SUPABASE_URL = "https://kxmnvtgnwuhdkrzzpwxi.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW52dGdud3VoZGtyenpwd3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTA3OTgsImV4cCI6MjA1NTI2Njc5OH0.l0DeaGtDKbr-EhNX5DpEUDSNtF1Y3L_Rdqn2bUC7JcA";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Periksa apakah window.supabase sudah terdefinisi
+if (window.supabase) {
+  const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  console.log('Supabase client berhasil diinisialisasi');
+} else {
+  console.error('Supabase tidak terdefinisi');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const mulaiButton = document.getElementById('mulai');
   // Menambahkan event listener untuk animasi tombol
