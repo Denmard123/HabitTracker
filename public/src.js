3e52d7d7bd83effe8e47caef7f864a3afbd75c0c
Denmard123 <dmardiyana123@gmail.com>
2025-02-18 03:02:20 +0700

    --

--- public/src.js
+++ public/src.js
@@ -1,4 +1,4 @@
-import { supabase } from '../supabaseClient';
+import { supabase } from './supabaseClient';
 document.addEventListener('DOMContentLoaded', () => {
   const mulaiButton = document.getElementById('mulai');
 
