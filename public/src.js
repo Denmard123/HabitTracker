469a1a1a2f20d0dd5fd466b0ec27c0b6944f3d95
Denmard123 <dmardiyana123@gmail.com>
2025-02-18 03:16:16 +0700

    -

--- public/src.js
+++ public/src.js
@@ -2,7 +2,7 @@
   // Inisialisasi Supabase Client
   const SUPABASE_URL = 'https://kxmnvtgnwuhdkrzzpwxi.supabase.co';
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW52dGdud3VoZGtyenpwd3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTA3OTgsImV4cCI6MjA1NTI2Njc5OH0.l0DeaGtDKbr-EhNX5DpEUDSNtF1Y3L_Rdqn2bUC7JcA';
-  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
   async function fetchData() {
     const { data, error } = await supabase
       .from('HT')
