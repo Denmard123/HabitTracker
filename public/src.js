f18dcb82bc7f0c3acd9310b5e5f565afc9af8df9
den mardiyana saputra <63204258+Denmard123@users.noreply.github.com>
2025-02-18 14:55:59 +0700

    Update src.js

--- public/src.js
+++ public/src.js
@@ -1,5 +1,5 @@
 // Inisialisasi Supabase Client
-const SUPABASE_URL = "https://kxmnvtgnwuhdkrzzpwxi.supabase.co";
+const SUPABASE_URL = "https://kxmnvtgnwuhdkrzzpwxi.supabase.co/rest/v1/HT?select=*";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW52dGdud3VoZGtyenpwd3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTA3OTgsImV4cCI6MjA1NTI2Njc5OH0.l0DeaGtDKbr-EhNX5DpEUDSNtF1Y3L_Rdqn2bUC7JcA";
 
 // Pastikan Supabase sudah di-load sebelum digunakan
