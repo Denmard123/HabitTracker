1f657d24949d08865174f276bc4ac616fa3528da
Denmard123 <dmardiyana123@gmail.com>
2025-08-26 01:15:32 +0700

    import next.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ const SUPABASE_URL = 'https://kxmnvtgnwuhdkrzzpwxi.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW52dGdud3VoZGtyenpwd3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTA3OTgsImV4cCI6MjA1NTI2Njc5OH0.l0DeaGtDKbr-EhNX5DpEUDSNtF1Y3L_Rdqn2bUC7JcA';
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
+
 app.use(express.json());
 
 // Melayani file statis (HTML, CSS, JS)
